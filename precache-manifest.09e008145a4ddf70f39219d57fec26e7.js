0f485ef8ae5ff6201b913fafd3a49266a5a75749
Simmi <simmiagrawal49@gmail.com>
2021-04-24 01:04:36 +0530

    Updates

---
+++ precache-manifest.09e008145a4ddf70f39219d57fec26e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03471dd9733056cf9d18445a17463efd",
+    "url": "/weather-ghpages/index.html"
+  },
+  {
+    "revision": "3ca2f93ef790528b521f",
+    "url": "/weather-ghpages/static/css/main.1f4fb573.chunk.css"
+  },
+  {
+    "revision": "5dbe35b63cb2a558660b",
+    "url": "/weather-ghpages/static/js/2.9141df50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-ghpages/static/js/2.9141df50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca2f93ef790528b521f",
+    "url": "/weather-ghpages/static/js/main.11115d1b.chunk.js"
+  },
+  {
+    "revision": "05ebfa9dd20632181ced",
+    "url": "/weather-ghpages/static/js/runtime-main.0aceb482.js"
+  },
+  {
+    "revision": "eb0cc7e48d460fca7b3932df96f0d0e5",
+    "url": "/weather-ghpages/static/media/cold.eb0cc7e4.jpg"
+  }
+]);
--- src/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  API_KEY: "4dd59386cb24737bda58add8740170ee",
-  BASE_URL: "https://api.openweathermap.org/data/2.5/",
-};
